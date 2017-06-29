11c0acc4c57d9824c8509b1aad250f6574d71f71
Christopher Hernandez <christohernandez011@gmail.com>
2017-06-28 20:13:36 -0400

    Adding all initial files

---
+++ app/assets/javascripts/todo_items.js
@@ -0,0 +1,5 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
